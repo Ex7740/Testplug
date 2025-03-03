93b1cb57adbd446491ed10550e84ad75d08fe973
Ex7740 <dexterjeal@gmail.com>
2025-03-03 19:08:20 +0000

    Updated

--- lua/plugins/my_plugin.lua
+++ lua/plugins/my_plugin.lua
@@ -3,13 +3,6 @@ return {
         "Ex7740/my_plugin",  -- Replace with your actual GitHub username and repo
         lazy = false,  -- Set to true if you want to lazy-load it on a specific event
         config = function()
-            -- Set a keybinding to open the floating window
-            vim.api.nvim_set_keymap(
-                "n", 
-                "<leader>fw",  -- Keybinding
-                ":lua require('my_plugin').open_floating_window()<CR>",  -- Command to open the window
-                { noremap = true, silent = true }
-            )
         end,
     },
 }
